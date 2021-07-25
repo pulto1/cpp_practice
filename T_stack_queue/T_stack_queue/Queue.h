84083d25cc871e3d45bb3e20257743ef13ca031d
pulto <1016828439@qq.com>
2021-07-25 13:56:05 +0800

    栈和队列模拟实现

--- T_stack_queue/T_stack_queue/Queue.h
+++ T_stack_queue/T_stack_queue/Queue.h
@@ -1,1 +1,56 @@
 #pragma once
+
+#include <queue>
+
+using namespace std;
+
+namespace bit
+{
+    template<class T, class Con = deque<T>>
+    class queue
+    {
+    public:
+        void push(const T& x)
+        {
+            _c.push_back();
+        }
+
+        void pop()
+        {
+            _c.pop_front();
+        }
+
+        T& back()
+        {
+            _c.back();
+        }
+
+        const T& back()const
+        {
+            _c.back();
+        }
+
+        T& front()
+        {
+            _c.front();
+        }
+
+        const T& front()const
+        {
+            _c.front();
+        }
+
+        size_t size()const
+        {
+            _c.size();
+        }
+
+        bool empty()const
+        {
+            _c.empty();
+        }
+    private:
+        Con _c;
+
+    };
+}
--- T_stack_queue/T_stack_queue/Stack.h
+++ T_stack_queue/T_stack_queue/Stack.h
@@ -1,1 +1,47 @@
 #pragma once
+
+#include<iostream>
+#include <queue>
+
+using namespace std;
+
+namespace bit
+{
+    template<class T, class Con = deque<T>>
+    class stack
+    {
+    public:
+        void push(const T& x)
+        {
+            _c.push_back();
+        }
+
+        void pop()
+        {
+            _c.pop_back();
+        }
+
+        T& top()
+        {
+            return _c.back();
+        }
+
+        const T& top()const
+        {
+            return _c.back();
+        }
+
+        size_t size()const
+        {
+            return _c.size();
+        }
+
+        bool empty()const
+        {
+            return _c.empty();
+        }
+    
+    private:
+        Con _c;
+    };
+}
